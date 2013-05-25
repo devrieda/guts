6c9c3a2350390beee979236e5198c15056ffb669
Derek DeVries <devrieda@gmail.com>
2013-05-25 12:22:14 -0600

    strip out script, style, and comments from them html

--- lib/guts/document.rb
+++ lib/guts/document.rb
@@ -1,9 +1,36 @@
 module Guts
   class Document
-    attr_accessor :html
+    attr_accessor :html, :content
 
     def initialize(html)
-      @html = html
+      @html    = html
+      @content = clean(html)
+    end
+
+    private
+
+    def clean(html)
+      sanitizer.clean(html, :elements        => accepted_elements,
+                            :remove_contents => true)
+    end
+
+    def sanitizer(sanitizer = Sanitize)
+      @sanitizer = sanitizer
+    end
+
+    def accepted_elements
+      %w[
+        a abbr acronym address applet area article aside audio b base basefont
+        bdi bdo big blockquote body br button canvas caption center cite code
+        col colgroup command datalist dd del details dfn dialog dir div dl dt
+        em embed fieldset figcaption figure font footer form frame frameset h1
+        h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd
+        keygen label legend li link map mark menu meta meter nav noframes object
+        ol optgroup option output p param pre progress q rp rt ruby s samp
+        section select small source span strike strong sub summary sup
+        table tbody td textarea tfoot th thead time title tr track tt u ul var
+        video wbr
+      ]
     end
   end
 end
--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -9,4 +9,68 @@ describe Document do
       expect(doc.html).to eq html
     end
   end
+
+  describe "#content" do
+    describe "with styles" do
+      let(:html) { read_fixture("strip_style.html") }
+      let(:doc)  { Document.new(html) }
+
+      it "should strip out style tags" do
+        expect(doc.html).to include("<style")
+        expect(doc.content).to_not include("<style")
+      end
+
+      it "should strip out content within style tags" do
+        expect(doc.html).to include(".wp-polls")
+        expect(doc.content).to_not include(".wp-polls")
+      end
+    end
+
+    describe "with script" do
+      let(:html) { read_fixture("strip_script.html") }
+      let(:doc)  { Document.new(html) }
+
+      it "should strip out script tags" do
+        expect(doc.html).to include("<script")
+        expect(doc.content).to_not include("<script")
+      end
+
+      it "should strip out content within script tags" do
+        expect(doc.html).to include("function() {")
+        expect(doc.content).to_not include("function() {")
+      end
+
+      it "should strip out noscript tags" do
+        expect(doc.html).to include("<noscript")
+        expect(doc.content).to_not include("<noscript")
+      end
+    end
+
+    describe "with comments" do
+      let(:html) { read_fixture("strip_comments.html") }
+      let(:doc)  { Document.new(html) }
+
+      it "should strip out html comments" do
+        expect(doc.html).to include("<!--")
+        expect(doc.html).to include("-->")
+
+        expect(doc.content).to_not include("<!--")
+        expect(doc.content).to_not include("-->")
+      end
+
+      it "should keep non-commented code" do
+        expect(doc.html).to include("Alpha Bravo")
+        expect(doc.content).to include("Alpha Bravo")
+      end
+
+      it "should strip out commented markup" do
+        expect(doc.html).to include("Charlie Delta")
+        expect(doc.html).to include("Echo Foxtrot")
+
+        expect(doc.content).to_not include("Charlie Delta")
+        expect(doc.content).to_not include("Echo Foxtrot")
+      end
+    end
+  end
+
 end
