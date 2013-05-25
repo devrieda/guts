bcec9aa93ec3b1aee0a762c31f1f7a786cf52874
Derek DeVries <devrieda@gmail.com>
2013-05-25 11:45:38 -0600

    fix path to fuxture

--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Document do
   describe ".new" do
-    let(:html) { read_fixture("sports/airalamo.com") }
+    let(:html) { read_fixture("articles/airalamo.com") }
 
     it "should initialize content" do
       doc = Document.new(html)
