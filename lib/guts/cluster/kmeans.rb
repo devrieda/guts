307b4007b0c483ba39718f3f8391e81da47be78b
Derek DeVries <devrieda@gmail.com>
2013-06-16 14:04:33 -0600

    calculate kmeans

--- lib/guts/cluster/kmeans.rb
+++ lib/guts/cluster/kmeans.rb
@@ -10,4 +10,42 @@ class Kmeans
     end
   end
 
+  def calculate
+    begin
+      assign_points_to_clusters
+    end while max_delta >= @delta
+
+    clusters
+  end
+
+  private
+
+  def assign_points_to_clusters
+    @clusters.each {|cluster| cluster.clear_values! }
+
+    @values.each do |value|
+      closest = closest_cluster(value)
+      closest << value
+    end
+  end
+
+  def closest_cluster(value)
+    closest, min_dist = nil
+
+    @clusters.each do |cluster|
+      dist = (value - cluster.center).abs
+      min_dist ||= dist
+
+      if dist <= min_dist
+        min_dist, closest = dist, cluster
+      end
+    end
+
+    closest
+  end
+
+  def max_delta
+    @clusters.map {|c| c.recenter! }.max
+  end
+
 end
--- spec/cluster/kmeans_spec.rb
+++ spec/cluster/kmeans_spec.rb
@@ -10,4 +10,12 @@ describe Kmeans do
     end
   end
 
+  describe "#calculate" do
+    it "should assign points to the number of clusters specified" do
+      kmeans = Kmeans.new([1, 2, 3, 4, 5], 2, 0.001)
+
+      clusters = kmeans.calculate
+      expect(clusters.length).to eq 2
+    end
+  end
 end
