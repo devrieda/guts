f2928bab6fcc2a0cb1346c58b9d4b1d450e397fe
Derek DeVries <devrieda@gmail.com>
2013-06-16 14:02:44 -0600

    initialize new kmeans calculation

--- lib/guts/cluster/kmeans.rb
+++ lib/guts/cluster/kmeans.rb
@@ -1,4 +1,6 @@
 class Kmeans
+  attr_reader :values, :clusters
+
   def initialize(values, k, delta=0.001)
     @values = values
     @delta  = delta
@@ -7,4 +9,5 @@ class Kmeans
       Cluster.new(values.sample)
     end
   end
+
 end
--- spec/cluster/kmeans_spec.rb
+++ spec/cluster/kmeans_spec.rb
@@ -2,5 +2,12 @@ require 'spec_helper'
 
 describe Kmeans do
   describe ".new" do
+    it "should build initial clusters" do
+      kmeans = Kmeans.new([1, 2, 3, 4, 5], 2, 0.001)
+
+      expect(kmeans.values.length).to eq 5
+      expect(kmeans.clusters.length).to eq 2
+    end
   end
+
 end
